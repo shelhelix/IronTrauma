e322c16eeb017b31913ae89628206dfe7cb23e50
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2022-11-13 15:26:15 +0200

    added ballast tank to the submarine

---
+++ Assets/Scripts/Submarine/MovementSystem/SubmarineMovementSystem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace IronTrauma.Submarine.EngineSystem {
+	public class SubmarineMovementSystem : MonoBehaviour {
+		public BallastTank BallastTank;
+		public Rigidbody  SubmarineRigidbody;
+
+		public void FixedUpdate() {
+			SubmarineRigidbody.AddForce(SubmarineRigidbody.transform.up * BallastTank.PushPower, ForceMode.Impulse);
+		}
+	}
+}
