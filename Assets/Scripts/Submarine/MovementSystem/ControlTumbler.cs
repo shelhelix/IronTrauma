98a5bbf4894ec12dcac98a5d8c68828c039a99b6
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2022-11-22 19:54:45 +0200

    unified submarine controls

---
+++ Assets/Scripts/Submarine/MovementSystem/ControlTumbler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace IronTrauma.Submarine.EngineSystem {
+	public class ControlTumbler : MonoBehaviour {
+		public HingeJoint Joint;
+		public float      Min;
+		public float      Max;
+
+		public float CurValue => NormalizedJointPosition * (Max - Min) + Min;
+		
+		protected float NormalizedJointPosition => (Joint.angle - Joint.limits.min) / (Joint.limits.max - Joint.limits.min); 
+	}
+}
--- Assets/Scripts/Submarine/MovementSystem/SubmarineMovementSystem.cs
+++ Assets/Scripts/Submarine/MovementSystem/SubmarineMovementSystem.cs
@@ -4,18 +4,20 @@ namespace IronTrauma.Submarine.EngineSystem {
 	public class SubmarineMovementSystem : MonoBehaviour {
 		public Rigidbody  SubmarineRigidbody;
 
-		public BallastTankTumbler     BallastTankTumbler;
-		public RotationTumbler RotationTumbler;
-		public EngineTumbler   EngineTumbler;
+		public ControlTumbler BallastTankControlTumbler;
+		public ControlTumbler RotationTumbler;
+		public ControlTumbler EngineTumbler;
 		
 		public void FixedUpdate() {
-			SubmarineRigidbody.AddForce(SubmarineRigidbody.transform.up * BallastTankTumbler.PushPower, ForceMode.Impulse);
-			SubmarineRigidbody.AddForce(SubmarineRigidbody.transform.forward * EngineTumbler.CurThrust, ForceMode.Impulse);
+			print($"UP: {BallastTankControlTumbler.CurValue}, Forward: {EngineTumbler.CurValue}, rotation: {RotationTumbler.CurValue}");
+			
+			SubmarineRigidbody.AddForce(SubmarineRigidbody.transform.up * BallastTankControlTumbler.CurValue, ForceMode.Impulse);
+			SubmarineRigidbody.AddForce(SubmarineRigidbody.transform.forward * EngineTumbler.CurValue, ForceMode.Impulse);
 			RotateSubmarine();
 		}
 
 		void RotateSubmarine() {
-			var angleSpeedInRad = RotationTumbler.CurAngularSpeed * Mathf.Deg2Rad;
+			var angleSpeedInRad = RotationTumbler.CurValue * Mathf.Deg2Rad;
 			SubmarineRigidbody.angularVelocity = new Vector3(0, angleSpeedInRad, 0);
 		}
 	}
